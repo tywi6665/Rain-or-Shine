3aaa5dbb560526052a3f87571c6682c0d2714a57
tywi6665 <tyletcole@gmail.com>
2019-05-29 20:46:02 -0600

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from "./components/Container";
 import WeatherIcon from "./components/WeatherIcon";
 import WeatherDetail from "./components/WeatherDetail";
@@ -6,31 +6,46 @@ import API from "./utils/API";
 import './App.scss';
 
 function App() {
-  API.getWeather().then(res => console.log(res.data))
+
+  const [currentWeather, setCurrentWeather] = useState(null)
+
+  useEffect(() => {
+    API.getWeather()
+      .then(res => setCurrentWeather(res.data.currently))
+  }, []);
+
+  console.log(currentWeather)
+
   return (
     <div className="App">
       <Container>
         <p>Welcome to Rain or Shine!</p>
       </Container>
-      <Container>
-        <WeatherIcon
-          icon={"chanceflurries"}
-        />
-      </Container>
-      <Container>
-        <WeatherDetail
-          weatherCondition={"Chance of rain"}
-          weatherInfo={"90%"}
-        />
-        <WeatherDetail
-          weatherCondition={"Wind Speed"}
-          weatherInfo={"25mph"}
-        />
-        <WeatherDetail
-          weatherCondition={"Wind Direction"}
-          weatherInfo={"NNW"}
-        />
-      </Container>
+      {currentWeather ? (
+        <>
+          <Container>
+            <WeatherIcon
+              icon={currentWeather.icon}
+            />
+          </Container>
+          <Container>
+            <WeatherDetail
+              weatherCondition={"Temperature"}
+              weatherInfo={currentWeather.temperature}
+            />
+            <WeatherDetail
+              weatherCondition={"Wind Speed"}
+              weatherInfo={currentWeather.windSpeed}
+            />
+            <WeatherDetail
+              weatherCondition={"Wind Direction"}
+              weatherInfo={"NNW"}
+            />
+          </Container>
+        </>
+      ) : (
+          <p>Retrieving Weather</p>
+        )}
     </div>
   );
 }
