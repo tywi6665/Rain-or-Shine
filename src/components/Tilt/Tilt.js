f75d08303e65c3f5468912245dc89f64434cb387
tywi6665 <tyletcole@gmail.com>
2019-06-01 10:08:10 -0600

    creating useState hooks in Tilt

--- src/components/Tilt/Tilt.js
+++ src/components/Tilt/Tilt.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { getPosition } from "./utils/getPosition";
 
 const Tilt = ({ children }) => {
 
+    const [position, setPosition] = useState({ x: 0, y: 0});
+    const [transitionTime, setTranistionTime] = useState(null)
 
     return (
         <div className="tilt">
