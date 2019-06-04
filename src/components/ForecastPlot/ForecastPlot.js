d9b029c8c46e07d59be2a7a79c31676cecdff7e3
tywi6665 <tyletcole@gmail.com>
2019-06-03 20:49:43 -0600

    adding useState hook

--- src/components/ForecastPlot/ForecastPlot.js
+++ src/components/ForecastPlot/ForecastPlot.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as d3 from "d3";
 import "./ForecastPlot.scss";
 
 const ForecastPlot = (props) => {
-    console.log(props.forecast)
+
+    const [forecastData, setForecastData] = useState(null)
+
+    useEffect(() => {
+
+        if (props.forecast) {
+            const rawForecastData = props.forecast;
+
+            let forecastDataArr = [];
+
+            rawForecastData.forEach((data) => {
+                forecastDataArr.push({
+                    date: data.time,
+                    highTemp: data.temperatureHigh,
+                    highTempTime: data.temperatureHighTime,
+                    lowTemp: data.temperatureLow,
+                    lowTempTime: data.temperatureLowTime
+                })
+            });
+            setForecastData(forecastDataArr);
+        }
+    }, [props.forecast])
+
+    console.log(forecastData);
 
     const margin = {
         top: 20,
@@ -21,7 +44,7 @@ const ForecastPlot = (props) => {
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     const x = d3.scaleTime()
         .range([0, width]);
@@ -33,12 +56,10 @@ const ForecastPlot = (props) => {
 
     const yAxis = d3.axisLeft(y);
 
-    const lineGenerator = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d) { return x(d.day); })
-        .y(function(d) { return y(d.temp); });
-
-    const forecastData = {};
+    const lineGenerator = d3.line()
+        .curve(d3.curveMonotoneX)
+        .x(function (d) { return x(d.day); })
+        .y(function (d) { return y(d.temp); });
 
     return (
         <div className="plot"></div>
