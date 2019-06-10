a4c8eaa980c26ec54aa1857c90854fd280b259e3
tywi6665 <tyletcole@gmail.com>
2019-06-10 13:46:36 -0600

    building out context provider

--- src/context/weatherContext.js
+++ src/context/weatherContext.js
@@ -1,13 +1,22 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import API from "../utils/API";
 
 export const weatherContext = createContext();
 
 export const weatherProvider = props => {
 
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        API.getWeather()
+            .then(res => {
+                setWeather(res.data);
+            })
+    }, []);
 
     return (
-        <WeatherContext.Provider>
+        <WeatherContext.Provider value={[weather, setWeather]}>
             {props.children}
         </WeatherContext.Provider>
-    )
-}
+    );
+};
