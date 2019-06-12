7bde159133f4a8930fcb956dc49b92648a4023bf
tywi6665 <tyletcole@gmail.com>
2019-06-12 08:02:03 -0600

    added background component

---
+++ src/components/Background/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Background.scss";
+
+const Background = (props) => {
+    return ( 
+        <div className="backgroundImage">
+            
+        </div>
+     );
+}
+ 
+export default Background;
