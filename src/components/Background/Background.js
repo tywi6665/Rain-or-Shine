da55dc44cf0e5c98d6ed5611b0548bede9a176bb
tywi6665 <tyletcole@gmail.com>
2019-06-12 09:34:57 -0600

    added some more mountains

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -2,16 +2,22 @@ import React from 'react';
 import "./Background.scss";
 
 const Background = (props) => {
-    return ( 
+    return (
         <div className="backgroundImage">
             <div className="mountains">
+                <div className="large-mountain-farleft"></div>
+                <div className="large-mountain-farright"></div>
                 <div className="small-mountain-left"></div>
                 <div className="small-mountain-right"></div>
-                <div className="mountain-left"></div>
-                <div className="mountain-right"></div>
+                <div className="medium-mountain-left"></div>
+                <div className="medium-mountain-right"></div>
+                <div className="large-mountain-left"></div>
+                <div className="large-mountain-right"></div>
+                <div className="medium-mountain-farleft"></div>
+                <div className="medium-mountain-farright"></div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Background;
