7e2d9bf51bb47610805c60f3efb7a69455abb79d
tywi6665 <tyletcole@gmail.com>
2019-06-12 19:03:01 -0600

    added rays to sun

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -12,22 +12,35 @@ const Background = ({ dayOrNight }) => {
                     <div className="moon-crater"></div>
                 </div>
             ) : (
-                <div className="sun"></div>
+                <div className="sun">
+                    <div className="ray-container">
+                        <div className="ray ray1"></div>
+                        <div className="ray ray2"></div>
+                        <div className="ray ray3"></div>
+                        <div className="ray ray4"></div>
+                        <div className="ray ray5"></div>
+                        <div className="ray ray6"></div>
+                        <div className="ray ray7"></div>
+                        <div className="ray ray8"></div>
+                        <div className="ray ray9"></div>
+                        <div className="ray ray10"></div>
+                    </div>
+                </div>
             )}
             <div className="mountains">
-                <div className="large-mountain-farleft"></div>
-                <div className="large-mountain-farright"></div>
-                <div className="small-mountain-left"></div>
-                <div className="small-mountain-right"></div>
-                <div className="medium-mountain-left"></div>
-                <div className="medium-mountain-right"></div>
-                <div className="large-mountain-left"></div>
-                <div className="large-mountain-right"></div>
-                <div className="medium-mountain-farleft"></div>
-                <div className="medium-mountain-farright"></div>
-            </div>
-        </div>
-    );
-}
-
+                            <div className="large-mountain-farleft"></div>
+                            <div className="large-mountain-farright"></div>
+                            <div className="small-mountain-left"></div>
+                            <div className="small-mountain-right"></div>
+                            <div className="medium-mountain-left"></div>
+                            <div className="medium-mountain-right"></div>
+                            <div className="large-mountain-left"></div>
+                            <div className="large-mountain-right"></div>
+                            <div className="medium-mountain-farleft"></div>
+                            <div className="medium-mountain-farright"></div>
+                        </div>
+                    </div>
+                );
+            }
+            
 export default Background;
