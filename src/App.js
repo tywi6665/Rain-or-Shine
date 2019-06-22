52e17ad8168bf5d64d9effe0036b93e226dbb340
tywi6665 <tyletcole@gmail.com>
2019-06-22 12:22:06 -0600

    added shelf clouds and nested ternary

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ function App() {
       {dayOrNight ? (
         <Background
           dayOrNight={dayOrNight}
+          currentWeather={"cloudy"}
         />
       ) : (
           ""
--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,13 +1,65 @@
 import React from 'react';
 import "./Background.scss";
 
-const Background = ({ dayOrNight }) => {
-    console.log(dayOrNight)
+const Background = ({ dayOrNight, currentWeather }) => {
+    console.log(currentWeather);
     return (
-        <div className={`backgroundImage ${dayOrNight}`}>
-            <div className="sky"></div>
-            {dayOrNight === "night" ? (
-               <>
+        <div className={`backgroundImage ${dayOrNight} ${currentWeather}-background`}>
+            {currentWeather === "cloudy" || currentWeather === "rain" ? (
+                <>
+                    <div
+                        className={`sky ${currentWeather}`}
+                        style={{ background: "linear-gradient(to bottom, darkgrey, #ffffff)" }}
+                    ></div>
+                    <div className="shelf-cloud-left"></div>
+                    <div className="shelf-cloud-right"></div>
+                </>
+            ) : [
+                    <div
+                        className="sky"
+                        style={{ background: "linear-gradient(to bottom, #845EC2, #D65DB1, #FF6F91, #FF9671, #FFC75F, #F9F871)" }}
+                    ></div>,
+                    (dayOrNight === "night" ? (
+                        <>
+                            <div className="stars">
+                                <div className="star-group1"></div>
+                                <div className="star-group2"></div>
+                                <div className="star-group3"></div>
+                            </div>
+                            <div className="moon">
+                                <div className="crater crater1"></div>
+                                <div className="crater crater2"></div>
+                                <div className="crater crater3"></div>
+                            </div>
+                        </>
+                    ) : (
+                            currentWeather === "cloudy" || currentWeather === "rain" ? (
+                                null
+                            ) : (
+                                    <div className="sun">
+                                        <div className="ray-container">
+                                            <div className="ray ray1"></div>
+                                            <div className="ray ray2"></div>
+                                            <div className="ray ray3"></div>
+                                            <div className="ray ray4"></div>
+                                            <div className="ray ray5"></div>
+                                            <div className="ray ray6"></div>
+                                            <div className="ray ray7"></div>
+                                            <div className="ray ray8"></div>
+                                            <div className="ray ray9"></div>
+                                            <div className="ray ray10"></div>
+                                        </div>
+                                    </div>
+                                )
+
+                        ))
+                ]}
+            {/* <div 
+                className="sky"
+                style={{background: "linear-gradient(to bottom, #845EC2, #D65DB1, #FF6F91, #FF9671, #FFC75F, #F9F871)"}}
+            ></div> */}
+            {/* {dayOrNight === "night" ? (
+                <>
                     <div className="stars">
                         <div className="star-group1"></div>
                         <div className="star-group2"></div>
@@ -20,21 +72,26 @@ const Background = ({ dayOrNight }) => {
                     </div>
                 </>
             ) : (
-                    <div className="sun">
-                        <div className="ray-container">
-                            <div className="ray ray1"></div>
-                            <div className="ray ray2"></div>
-                            <div className="ray ray3"></div>
-                            <div className="ray ray4"></div>
-                            <div className="ray ray5"></div>
-                            <div className="ray ray6"></div>
-                            <div className="ray ray7"></div>
-                            <div className="ray ray8"></div>
-                            <div className="ray ray9"></div>
-                            <div className="ray ray10"></div>
-                        </div>
-                    </div>
-                )}
+                    currentWeather === "cloudy" || currentWeather === "rain" ? (
+                        null
+                    ) : (
+                            <div className="sun">
+                                <div className="ray-container">
+                                    <div className="ray ray1"></div>
+                                    <div className="ray ray2"></div>
+                                    <div className="ray ray3"></div>
+                                    <div className="ray ray4"></div>
+                                    <div className="ray ray5"></div>
+                                    <div className="ray ray6"></div>
+                                    <div className="ray ray7"></div>
+                                    <div className="ray ray8"></div>
+                                    <div className="ray ray9"></div>
+                                    <div className="ray ray10"></div>
+                                </div>
+                            </div>
+                        )
+
+                )} */}
             <div className="clouds">
                 <div className="cloud small-cloud cloud1"></div>
                 <div className="cloud small-cloud cloud2"></div>
