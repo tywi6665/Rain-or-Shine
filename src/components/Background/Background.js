972b44287733c65a7fa3c13b6c14e9a67c24e818
tywi6665 <tyletcole@gmail.com>
2019-06-22 18:44:33 -0600

    refactoring rain element generation

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -2,10 +2,25 @@ import React from 'react';
 import "./Background.scss";
 
 const Background = ({ dayOrNight, currentWeather }) => {
-    
+
+    function rainOrSnow() {
+        const length = 150;
+        let array = [...Array(length).keys()];
+        return (
+            <div className={`${currentWeather}ing`}>
+                {
+                    array.map(index => (
+                        <i className={currentWeather} key={index}></i>
+                    ))
+                }
+            </div>
+
+        );
+    };
+
     return (
         <div className={`backgroundImage ${dayOrNight} ${currentWeather}-background`}>
-            {currentWeather === "cloudy" || currentWeather === "rain" || currentWeather === "snow" ? [
+            {currentWeather === "cloudy" || currentWeather === "rain" || currentWeather === "snow" || currentWeather === "fog" ? [
                 (<>
                     <div
                         className="sky"
@@ -17,59 +32,7 @@ const Background = ({ dayOrNight, currentWeather }) => {
                     </div>
                 </>),
                 (currentWeather === "rain" ? (
-                    //Map through rain divs
-                    <div className={`${currentWeather}ing`}>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                        <i className={currentWeather}></i>
-                    </div>
+                    rainOrSnow()
                 ) : (
                         null
                     ))
@@ -93,7 +56,7 @@ const Background = ({ dayOrNight, currentWeather }) => {
                             </div>
                         </>
                     ) : (
-                            currentWeather === "cloudy" || currentWeather === {currentWeather} ? (
+                            currentWeather === "cloudy" || currentWeather === { currentWeather } ? (
                                 null
                             ) : (
                                     <div className="sun">
@@ -114,44 +77,6 @@ const Background = ({ dayOrNight, currentWeather }) => {
 
                         ))
                 ]}
-            {/* <div 
-                className="sky"
-                style={{background: "linear-gradient(to bottom, #845EC2, #D65DB1, #FF6F91, #FF9671, #FFC75F, #F9F871)"}}
-            ></div> */}
-            {/* {dayOrNight === "night" ? (
-                <>
-                    <div className="stars">
-                        <div className="star-group1"></div>
-                        <div className="star-group2"></div>
-                        <div className="star-group3"></div>
-                    </div>
-                    <div className="moon">
-                        <div className="crater crater1"></div>
-                        <div className="crater crater2"></div>
-                        <div className="crater crater3"></div>
-                    </div>
-                </>
-            ) : (
-                    currentWeather === "cloudy" || currentWeather === {currentWeather} ? (
-                        null
-                    ) : (
-                            <div className="sun">
-                                <div className="ray-container">
-                                    <div className="ray ray1"></div>
-                                    <div className="ray ray2"></div>
-                                    <div className="ray ray3"></div>
-                                    <div className="ray ray4"></div>
-                                    <div className="ray ray5"></div>
-                                    <div className="ray ray6"></div>
-                                    <div className="ray ray7"></div>
-                                    <div className="ray ray8"></div>
-                                    <div className="ray ray9"></div>
-                                    <div className="ray ray10"></div>
-                                </div>
-                            </div>
-                        )
-
-                )} */}
             <div className="clouds">
                 <div className="cloud small-cloud cloud1"></div>
                 <div className="cloud small-cloud cloud2"></div>
